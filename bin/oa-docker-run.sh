fbcbfe26aad84b702e8d412fb03bcc180f5a729f
Ricardo Marques <rimarques@suse.com>
2017-02-24 13:56:08 +0000

    salt-master, salt-minion and DeepSea are now installed on the salt VM (instead of being installed on openattic-docker)
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- bin/oa-docker-run.sh
+++ bin/oa-docker-run.sh
@@ -1,13 +1,5 @@
-# TODO destroy ceph cluster if exists
-
-for i in 1 2 3; do
-  ssh node$i 'sudo rm /etc/salt/pki/minion/minion_master.pub'
-  ssh node$i 'sudo systemctl restart salt-minion'
-done
-
 sudo docker run -t \
   -v /home/vagrant/openattic:/srv/openattic \
-  -v /home/vagrant/DeepSea:/srv/deepsea \
   -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
   -v /etc/ceph:/etc/ceph \
   --net=host \
